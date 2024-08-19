58272c5edad14428563c095b83aeb68c8134893f
Webskitters <vikashofficial9@gmail.com>
2024-08-19 13:11:46 +0530

    Interaction with smart contract

--- 1_basic_interaction/index.js
+++ 1_basic_interaction/index.js
@@ -1,3 +1,41 @@
+const contractAddr = "0x337AfcC9d6Fea28DCee7CA5E667595B2f9973175"
+const contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_number",
+				"type": "uint256"
+			}
+		],
+		"name": "updateNumber",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "number",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
 async function callContract() {
-    console.log("a")
+    const provider = new ethers.provider.Web3(window.ethereum)
+    const numContract = new ethers.Contract(contractAddr, contractAbi, provider.getSigner())
+
+    //Writing the contract
+    await numContract.updateNumber(54)
+
+    //Reading the contract
+    const num = await numContract.number()
+    console.log(num.toString())
 }
