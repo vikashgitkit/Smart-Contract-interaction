8bc83ea2f4c826ca79e16a4ac450a9da88fac0a4
Vikash <vikashofficial9@gmail.com>
2024-08-20 18:59:22 +0530

    Sent eth to user

--- 2_ethers_course/contracts/Wallet.sol
+++ 2_ethers_course/contracts/Wallet.sol
@@ -9,16 +9,22 @@ contract Wallet {
         num = _num;
     }
 
-    function getValue() public view returns (uint){
+    function getValue() public view returns (uint) {
         return num;
     }
 
-     //Function to send eth to this contract
-    function sendEthContract() public payable {
-
-    }
+    //Function to send eth to this contract
+    function sendEthContract() public payable {}
 
     function contractBalance() public view returns (uint) {
         return address(this).balance;
+    }
+    //Function to send eth to the user
+    function sendEthUser(address _user) public payable {
+        payable(_user).transfer(msg.value);
+    }
+
+    function accountBalance(address _address) public view returns (uint) {
+        return (_address).balance;
     }
 }
