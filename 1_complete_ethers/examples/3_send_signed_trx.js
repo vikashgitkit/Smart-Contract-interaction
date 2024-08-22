4d5233ba048b1fc883fcbcc52870c1393f6831cd
Webskitters <vikashofficial9@gmail.com>
2024-08-22 11:43:23 +0530

    Checking balances before transaction

--- 1_complete_ethers/examples/3_send_signed_trx.js
+++ 1_complete_ethers/examples/3_send_signed_trx.js
@@ -10,11 +10,24 @@ const pvtkey1 = '';
 const wallet = new ethers.Wallet(pvtkey1, provider);
 
 const main = async() => {
+
+    //Show acc1 bal before transfer
+    const senderBalBefore = await provider.getBalance(acc1);
+    //Show acc2 bal before transfer
+    const receiverBalBefore = await provider.getBalance(acc2);
+
     //Send ether from one acc to another
     const tx = await wallet.sendTransaction({
         to: acc2,
         value: ethers.parseEther("0.1")
     });
 
-    
+    //Wait for trx to be mined
+    await tx.wait();
+    console.log("Transaction mined:",tx);
+
+     //Show acc1 bal after transfer
+    //Show acc2 bal after transfer
+
+
 }
