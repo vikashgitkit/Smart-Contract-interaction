ce4a365e048a91b9248dab46cfd5169b34cfdae7
Webskitters <vikashofficial9@gmail.com>
2024-08-26 11:36:55 +0530

    Events

--- 1_complete_ethers/examples/5_contract_event_stream.js
+++ 1_complete_ethers/examples/5_contract_event_stream.js
@@ -8,3 +8,16 @@ const acc2 = '0x955B386F9A71106f8A138aA27a30aa91544E3bEc';
 
 const pvtkey1 = '0fdb8b68d4e081b1802cc9cb82f5bb3d19463d85e1b7afbe39a5bc1cd545bf09';
 const wallet = new ethers.Wallet(pvtkey1, provider);
+
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+
+    "event Transfer(address indexed from, address indexed to, uint amount)"
+];
+
+//Dai token contract address
+const contractAddr = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const contract = new ethers.Contract(contractAddr, ERC20_ABI, provider);
